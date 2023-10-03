1386a2ebb8a76412a452cd8d5eb5ffde4cb0db2e
Shea Lewis (Kai) <355659+kaidesu@users.noreply.github.com>
2023-10-03 01:09:46 -0700

    work on hero

--- app/page.js
+++ app/page.js
@@ -12,14 +12,14 @@ export default function Home() {
             <ul className="mt-10 list-none space-y-8">
               <li>
                 <div className="flex space-x-4">
-                  <div className="flex-shrink-0 h-12 w-12 bg-foreground text-background rounded-lg flex items-center justify-center">
+                  <div className="flex-shrink-0 h-12 w-12 bg-foreground text-background rounded-md rounded-es-2xl rounded-se-2xl flex items-center justify-center">
                     <SquareDashedBottomCode />
                   </div>
 
                   <div>
                     <h4 className="mb-2 text-lg font-bold leading-none">Embeddable</h4>
                     <p className="leading-6 p-0 text-foreground/80">
-                      Extend and build applications in <span>Go</span> utilizing <span>Ghost</span> as your scripting language.
+                      Extend and build applications in <span className="font-bold border-b-2 border-sky-400">Go</span> utilizing <span className="font-bold border-b-2 border-foreground">Ghost</span> as your scripting language.
                     </p>
                   </div>
                 </div>
@@ -27,7 +27,7 @@ export default function Home() {
 
               <li>
                 <div className="flex space-x-4">
-                  <div className="flex-shrink-0 h-12 w-12 bg-foreground text-background rounded-lg flex items-center justify-center">
+                  <div className="flex-shrink-0 h-12 w-12 bg-foreground text-background rounded-md rounded-es-2xl rounded-se-2xl flex items-center justify-center">
                     <FilterIcon />
                   </div>
 
@@ -42,7 +42,7 @@ export default function Home() {
 
               <li>
                 <div className="flex space-x-4">
-                  <div className="flex-shrink-0 h-12 w-12 bg-foreground text-background rounded-lg flex items-center justify-center">
+                  <div className="flex-shrink-0 h-12 w-12 bg-foreground text-background rounded-md rounded-es-2xl rounded-se-2xl flex items-center justify-center">
                     <BoxIcon />
                   </div>
 
