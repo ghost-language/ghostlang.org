3f98918b57b8a869f93e7b472c50a8fa136f2a0d
Shea Lewis (Kai) <355659+kaidesu@users.noreply.github.com>
2023-10-04 01:18:01 -0700

    layout tweak

--- app/docs/[slug]/page.js
+++ app/docs/[slug]/page.js
@@ -16,7 +16,7 @@ export default function DocPage({ params: { slug } }) {
   const MDXContent = useMDXComponent(page.body.code)
 
   return (
-    <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
+    <div className="flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
       <aside className="fixed top-14 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block">
         <ScrollArea className="relative overflow-hidden h-full py-6 pl-8 pr-6 lg:py-8">
           {Object.keys(navigation.links).map((section, idx) => (
