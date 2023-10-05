1b57223b8ef7ca567c65a1280b5c2dfa3c1ee768
Kai <355659+kaidesu@users.noreply.github.com>
2023-10-05 11:11:42 -0700

    Update page.js

--- app/x/ghost/page.js
+++ app/x/ghost/page.js
@@ -1,11 +1,7 @@
-export const metadata = {
-  'go-import': 'ghostlang.org/x/ghost git https://github.com/ghost-language/ghost',
-}
-
 export default function XGhost() {
   return (
     <>
-      ...
+      <meta name="go-import" content="ghostlang.org/x/ghost git https://github.com/ghost-language/ghost" />
     </>
   )
 }
