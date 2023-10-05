a4ed65cf6b33d3c63eb2dfd0e63942cbc51a9de3
Shea Lewis (Kai) <355659+kaidesu@users.noreply.github.com>
2023-10-05 01:29:42 -0700

    test

---
+++ app/x/ghost/page.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  'go-import': 'ghostlang.org/x/ghost git https://github.com/ghost-language/ghost.git',
+}
+
+export default function XGhost() {
+  return (
+    <>
+      ...
+    </>
+  )
+}
